3a3904ad09a57209c3b2ef416b9eb1781ebceadd
illyabezpaliy13 <110487623+illyabezpaliy13@users.noreply.github.com>
2023-03-17 22:56:28 +0200

    smal fix

--- js/index3.js
+++ js/index3.js
@@ -1,4 +1,5 @@
 const userName = "Олександро";
 const payment = "300";
-const letter = `Дякуємо, ${userName}! До сплати ${payment} гривень`
-console.log(letter)
+const letter = `Дякуємо, ${userName}! До сплати ${payment} гривень`;
+console.log(alert(letter));
+console.log(letter);
